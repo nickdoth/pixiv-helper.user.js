a2be7a3eb06a6b9e5cb8b2a7eafc904bd4797161
nickdoth <nick.doth@gmail.com>
2015-04-12 01:16:33 +0800

    Fix version: 1.1.2

--- userscript-header.js
+++ userscript-header.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Pixiv Helper
 // @namespace    http://gitcafe.com/nickdoth/pixiv-helper
-// @version      0.1.1
+// @version      1.1.2
 // @description  P站懒人补丁, 简化收藏和关注等操作.
 // @author       nickdoth <nick.doth@gmail.com>
 // @match        http://www.pixiv.net/member_illust.php*
