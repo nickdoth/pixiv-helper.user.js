3209cca226a6e487b1fe5a112f1985837fd11357
nick <nick@localhost.localdomain>
2015-05-02 14:16:17 +0800

    Add match rule: http://www.pixiv.net/member.php*

--- userscript-header.js
+++ userscript-header.js
@@ -5,5 +5,6 @@
 // @description  P站懒人补丁, 简化收藏和关注等操作.
 // @author       nickdoth <nick.doth@gmail.com>
 // @match        http://www.pixiv.net/member_illust.php*
+// @match       http://www.pixiv.net/member.php*
 // @grant        none
 // ==/UserScript==
