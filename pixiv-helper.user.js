a132bf5b700548bf3888f3d078e49453c8c13d5b
nick <nick@localhost.localdomain>
2015-05-02 14:18:10 +0800

    Add match rule: http://www.pixiv.net/member.php*

--- pixiv-helper.user.js
+++ pixiv-helper.user.js
@@ -5,6 +5,7 @@
 // @description  P站懒人补丁, 简化收藏和关注等操作.
 // @author       nickdoth <nick.doth@gmail.com>
 // @match        http://www.pixiv.net/member_illust.php*
+// @match       http://www.pixiv.net/member.php*
 // @grant        none
 // ==/UserScript==
 
